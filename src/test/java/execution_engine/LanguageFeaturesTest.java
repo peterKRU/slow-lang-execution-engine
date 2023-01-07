d464c23aa0af4fd3aa61a655cdd4e814177bd822
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-07 22:54:53 +0200

    test: add LanguageFeaturesTest

---
+++ src/test/java/execution_engine/LanguageFeaturesTest.java
@@ -0,0 +1,14 @@
+package execution_engine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LanguageFeaturesTest {
+
+	@Test
+	final void test() {
+		fail("Not yet implemented");
+	}
+
+}
