d7012283b534a8b13d23d73dc10ef8607a05ee81
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-07 22:56:25 +0200

    test: add LoggerTest

---
+++ src/test/java/execution_engine/LoggerTest.java
@@ -0,0 +1,24 @@
+package execution_engine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LoggerTest {
+
+	@Test
+	final void testLogger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testLog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testExportLog() {
+		fail("Not yet implemented");
+	}
+
+}
