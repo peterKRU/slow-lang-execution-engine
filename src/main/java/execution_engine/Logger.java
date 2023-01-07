9db5ad67d95313d6d04d0146a81dc4e55b2c2004
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-07 19:29:19 +0200

    feat: add exportLog method to Logger
    Add LogExporter as class field in Logger.
    Add exportLog method to Logger class, use LogExporter for the implementation.

--- src/main/java/execution_engine/Logger.java
+++ src/main/java/execution_engine/Logger.java
@@ -1,15 +1,18 @@
 package execution_engine;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Logger {
 	
 	private List<String> log;
+	private TextExporter logExporter;
 	
 	public Logger() {
 		
 		this.log = new ArrayList<String>();
+		this.logExporter = new LogExporter();
 	}
 	
 	public void log(String text) {
@@ -17,4 +20,15 @@ public class Logger {
 		log.add(text);
 	}
 	
+	public void exportLog(String fileName) throws IOException {
+		
+		String lines = "";
+		
+		for(String line : log) {
+			
+			lines += line + " ";
+		}
+		
+		logExporter.export(lines, fileName);
+	}
 }
