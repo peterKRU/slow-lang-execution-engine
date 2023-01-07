1d612aa23041cbddef11f0ad39a3dfa38392c676
Peter Krastanov <peter.krastanow@gmail.com>
2023-01-07 23:01:06 +0200

    test: add test cases to RunnerTest

--- src/test/java/execution_engine/RunnerTest.java
+++ src/test/java/execution_engine/RunnerTest.java
@@ -2,13 +2,18 @@ package execution_engine;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 class RunnerTest {
 
 	@Test
-	void test() {
+	void test() throws IOException {
 		assertTrue(true);
+		
+		String test = ResourceReader.readResource("test.txt");
+		System.out.println(test);
 	}
 
 }
