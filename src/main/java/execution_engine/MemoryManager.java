fd0279c6dab15d7b4c48268ed94e85d08fd71c95
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-01 16:10:42 +0200

    feat: make MemoryManager a Singleton

--- src/main/java/execution_engine/MemoryManager.java
+++ src/main/java/execution_engine/MemoryManager.java
@@ -1,31 +1,48 @@
 package execution_engine;
 
 public class MemoryManager implements InstructionsLoader, HeapCache {
-	
+
+	private static MemoryManager memoryManagerInstance;
 //	private MainBlock mainBlock;
 //	private ClassSpace classSpace;
 //	private Heap heap;
 //	private GarbageCollector garbageCollector;
 	
+	private MemoryManager() {
+		
+		
+	}
+	
+	public static MemoryManager getInstance() {
+
+		if (memoryManagerInstance == null) {
+
+			memoryManagerInstance = new MemoryManager();
+
+			return memoryManagerInstance;
+		} else {
+			return memoryManagerInstance;
+		}
+	}
+
 	private void invokeGarbageCollection() {
-		
-		
+
 	}
 
 	@Override
 	public int[] loadInstructions() {
-		
+
 		return null;
 	}
 
 	@Override
 	public void store(int[] object) {
-		
+
 	}
 
 	@Override
 	public int[] fetch(int objectId) {
-		
+
 		return null;
 	}
 }
