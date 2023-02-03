c847f86b4d689a6b0720e8262a81847be4d5e6aa
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-03 16:52:25 +0200

    feat: add Special executor group

---
+++ src/main/java/execution_engine/SpecialExecutors.java
@@ -0,0 +1,13 @@
+package execution_engine;
+
+import java.util.List;
+
+public class SpecialExecutors implements ExecutorGroup {
+
+	@Override
+	public List<AbstractExecutor> getExecutors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
