f63bb849619d88ebc8e67e6db6332ebf46d8b479
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-03 15:31:52 +0200

    test: add unit tests for MemoryManager

--- src/test/java/execution_engine/MemoryManagerTest.java
+++ src/test/java/execution_engine/MemoryManagerTest.java
@@ -2,19 +2,21 @@ package execution_engine;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MemoryManagerTest {
 
 	@Test
-	@DisplayName("Test getInstance method: should return the same instance")
-	final void testMemoryManagerSingleton() {
+	@DisplayName("Test loadInstructionsMethod: loaded instructions array size should be 28")
+	final void testMemoryManager() throws IOException {
 
-		MemoryManager memoryManagerInstance = MemoryManager.getInstance();
-		MemoryManager memoryManagerOtherInstance = MemoryManager.getInstance();
-
-		assertTrue(memoryManagerInstance == memoryManagerOtherInstance);
+		MemoryManager memoryManager = new MemoryManager("test_compiled.txt");
+		int[] instructions = memoryManager.loadInstructions();
+				
+		assertTrue(instructions.length == 28);
 	}
 
 }
