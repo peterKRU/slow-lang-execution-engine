0dc386275ef6624ab51ec2b9da604d2d45f06005
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-07 18:57:47 +0200

    feat: add method for storing a class in ClassSpace

--- src/main/java/execution_engine/ClassSpace.java
+++ src/main/java/execution_engine/ClassSpace.java
@@ -1,5 +1,13 @@
 package execution_engine;
 
-public class ClassSpace {
+import java.util.Map;
 
+public class ClassSpace {
+	
+	private Map<Integer, SlowLangClass> classRegister;
+	
+	public void loadClass(SlowLangClass slowLangClass) {
+		
+		classRegister.put(slowLangClass.getClassId(), slowLangClass);
+	}
 }
