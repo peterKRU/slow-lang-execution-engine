8b82556d7e266a06eaf822e8872e5abc0ac7a315
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-09 17:24:07 +0200

    feat: implement base features of Heap

--- src/main/java/execution_engine/Heap.java
+++ src/main/java/execution_engine/Heap.java
@@ -1,5 +1,27 @@
 package execution_engine;
 
-public class Heap {
+import java.util.HashMap;
+
+public class Heap implements ObjectCache{
+	
+	private HashMap<Integer, int[]> storage;
+	
+	public Heap(HashMap<Integer, int[]> storage) {
+		
+		this.storage = storage;
+	}
+	
+	@Override
+	public void store(int[] object) {
+		
+		int objectId = object[0];
+		storage.put(objectId, object);
+	}
+
+	@Override
+	public int[] fetch(int objectId) {
+		
+		return storage.get(objectId);
+	}
 
 }
