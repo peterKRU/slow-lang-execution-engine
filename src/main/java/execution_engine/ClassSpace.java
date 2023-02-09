31e1915f896b7a604c3f75ac9ef450cf4d84c7c4
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-09 22:02:23 +0200

    style: improve method names

--- src/main/java/execution_engine/ClassSpace.java
+++ src/main/java/execution_engine/ClassSpace.java
@@ -6,23 +6,23 @@ import java.util.List;
 import java.util.Map;
 
 public class ClassSpace {
-	
+
 	private static Map<Integer, SlowLangClass> classRegister = new HashMap<Integer, SlowLangClass>();
 	private static List<Integer> classIds = new ArrayList<Integer>();
-	
+
 	public void registerClass(SlowLangClass slowLangClass) {
-		
+
 		classIds.add(slowLangClass.getClassId());
 		classRegister.put(slowLangClass.getClassId(), slowLangClass);
 	}
-	
+
 	public SlowLangClass getClass(int classId) {
-		
+
 		return classRegister.get(classId);
 	}
-	
-	public List<Integer> getClassIds() {
-		
+
+	public List<Integer> getClassIdList() {
+
 		return classIds;
 	}
 }
