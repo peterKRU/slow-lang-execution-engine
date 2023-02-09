f080672911a49ddba5c9ebcf49496fcc1f278b43
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-09 22:06:50 +0200

    refactor: change default values

--- src/main/java/execution_engine/ConstantsTable.java
+++ src/main/java/execution_engine/ConstantsTable.java
@@ -2,7 +2,7 @@ package execution_engine;
 
 public class ConstantsTable {
 
-	public static final int DEFAULT_GLOBAL_STORAGE_SIZE = 100;
-	public static final int DEFAULT_STACK_SIZE = 100;
-	public static final int DEFAULT_LOCAL_STORAGE_SIZE = 100;
+	public static final int DEFAULT_GLOBAL_STORAGE_SIZE = 1000;
+	public static final int DEFAULT_STACK_SIZE = 1000;
+	public static final int DEFAULT_LOCAL_STORAGE_SIZE = 1000;
 }
