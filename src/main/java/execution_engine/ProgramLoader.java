ea0c7c26c52a83c856eae5a794481e760bab4c8b
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-09 17:28:34 +0200

    refactor: delegate class loading and method binding functionality to separate classes
    Delegate loadClasses() and bindMethods() methods to SlowLangClassLoader and MethodBinder classes.
    Remove MainExecutionBlock from fields.

--- src/main/java/execution_engine/ProgramLoader.java
+++ src/main/java/execution_engine/ProgramLoader.java
@@ -1,132 +1,60 @@
 package execution_engine;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Objects;
 
 public class ProgramLoader {
 
 	private BytecodeVerifier bytecodeVerifier;
-	private Heap heap;
-	private ClassSpace classSpace;
-	private int[] mainExecutionBlock;
+	private FileImporter fileImporter;
+	private SlowLangClassLoader classLoader;
+	private MethodBinder methodBinder;
+
 	private int[] instructions;
+
+	@SuppressWarnings("unused")
+	private ClassSpace classSpace;
+	@SuppressWarnings("unused")
 	private HashMap<Integer, Integer> methodRegister;
 
-	private FileImporter fileImporter;
 	private static String programName;
 
-	public ProgramLoader(BytecodeVerifier bytecodeVerifier, Heap heap, ClassSpace classSpace, int[] mainExecutionBlock,
-			int[] instructions, HashMap<Integer, Integer> methodRegister) {
+	public ProgramLoader(ClassSpace classSpace, int[] instructions, HashMap<Integer, Integer> methodRegister) {
 
-		this.bytecodeVerifier = bytecodeVerifier;
-		this.heap = heap;
-		this.classSpace = classSpace;
-		this.mainExecutionBlock = mainExecutionBlock;
+		bytecodeVerifier = new BytecodeVerifier();
+		fileImporter = new BytecodeImporter();
+
 		this.instructions = instructions;
+		this.classSpace = classSpace;
 		this.methodRegister = methodRegister;
 
-		this.fileImporter = new BytecodeImporter();
-
+		classLoader = new SlowLangClassLoader(classSpace);
+		methodBinder = new MethodBinder(instructions, classSpace, methodRegister);
 	}
 
 	public void loadProgram(String fileName) throws IOException {
 
 		instructions = loadProgramBytes(fileName);
 		programName = stripFileExtensions(fileName);
-		
+
 		Decompiler.decompile(instructions);
-		
+
 		int expectedProgramId = Objects.hashCode(programName);
 		int actualProgramId = instructions[0] - Bytecodes.MAIN;
-		
+
 		if (!bytecodeVerifier.verifyProgramId(expectedProgramId, actualProgramId)) {
 
 			throw new RuntimeException("Invalid bytecode");
 		}
 
-		for (int i : instructions) {
-
-			System.out.print(i + " ");
-		}
-
-		loadMainExecutionBlock(mainExecutionBlock, instructions);
-		bindMethods(methodRegister, mainExecutionBlock, instructions);
-
-		loadClasses(classSpace, instructions);
-		loadObjects(heap);
+		classLoader.loadClasses(instructions);
+		methodBinder.bindMethods();
 	}
 
 	private int[] loadProgramBytes(String fileName) throws IOException {
 
 		return fileImporter.importFile(fileName);
-	}
-
-	private void loadObjects(Heap heap) {
-
-	}
-
-	private void loadClasses(ClassSpace classSpace, int[] instructions) {
-		
-		
-		for(int i = 0; i < instructions.length; i++) {
-			
-			
-		}
-	}
-
-	private void loadMainExecutionBlock(int[] mainExecutionBlock, int[] instructions) {
-
-		List<Integer> mainExecutionBlockList = new ArrayList<Integer>();
-		int mainExecutionBlockEndCode = Objects.hashCode(programName) - Bytecodes.HALT;
-		
-		for (int i = 1; i < instructions.length; i++) {
-
-			int instruction = instructions[i];
-
-			if (instruction == mainExecutionBlockEndCode) {
-
-				break;
-			}
-
-			mainExecutionBlockList.add(instruction);
-		}
-
-		mainExecutionBlock = mainExecutionBlockList.stream().mapToInt(i -> i).toArray();
-	}
-	
-	private void bindMethods(HashMap<Integer, Integer> methodRegister, int[] mainExecutionBlock, int[] instructions) {
-
-		for (int i = 0; i < mainExecutionBlock.length; i++) {
-
-			int instruction = mainExecutionBlock[i];
-
-			if (instruction == Bytecodes.CALL) {
-
-				int methodId = mainExecutionBlock[i + 1];
-				methodRegister.put(methodId, -1);
-			}
-		}
-
-		for (int i = mainExecutionBlock.length; i < instructions.length; i++) {
-
-			int instruction = instructions[i];
-
-			if (methodRegister.containsKey(instruction)) {
-
-				if (methodRegister.get(instruction) == -1) {
-
-					methodRegister.put(instruction, i + 1);
-				}
-			}
-		}
-	}
-
-	private static String getProgramName() {
-
-		return programName;
 	}
 
 	private static String stripFileExtensions(String fileName) {
@@ -138,5 +66,5 @@ public class ProgramLoader {
 
 		return fileName;
 	}
-	
+
 }
