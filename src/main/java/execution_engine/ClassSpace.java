8cb3172a0608f8bec3f5fc37a48b4a62db170ad2
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-09 17:23:17 +0200

    feat: add new features to ClassSpace

--- src/main/java/execution_engine/ClassSpace.java
+++ src/main/java/execution_engine/ClassSpace.java
@@ -1,13 +1,28 @@
 package execution_engine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ClassSpace {
 	
-	private Map<Integer, SlowLangClass> classRegister;
+	private static Map<Integer, SlowLangClass> classRegister = new HashMap<Integer, SlowLangClass>();
+	private static List<Integer> classIds = new ArrayList<Integer>();
 	
-	public void loadClass(SlowLangClass slowLangClass) {
+	public void registerClass(SlowLangClass slowLangClass) {
 		
+		classIds.add(slowLangClass.getClassId());
 		classRegister.put(slowLangClass.getClassId(), slowLangClass);
+	}
+	
+	public SlowLangClass getClass(int classId) {
+		
+		return classRegister.get(classId);
+	}
+	
+	public List<Integer> getClassIds() {
+		
+		return classIds;
 	}
 }
