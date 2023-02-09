75153f797afe31b452a70190d498da9a02893214
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-09 17:32:21 +0200

    refactor: change class methods metadata storing mechanism

--- src/main/java/execution_engine/SlowLangClass.java
+++ src/main/java/execution_engine/SlowLangClass.java
@@ -1,24 +1,38 @@
 package execution_engine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SlowLangClass {
-	
+
 	private int classId;
-	private List<List<Integer>> classMethods;
-	
-	public SlowLangClass(int classId, List<List<Integer>> classMethods) {
-		
+	private List<List<Integer>> methodAddresses;
+
+	public SlowLangClass(int classId, List<List<Integer>> methodAddresses) {
+
 		this.classId = classId;
-		this.classMethods = classMethods;
+		this.methodAddresses = methodAddresses;
 	}
-	
+
+	public SlowLangClass(int classId) {
+
+		this.classId = classId;
+		this.methodAddresses = new ArrayList<List<Integer>>();
+	}
+
 	public int getClassId() {
+
 		return classId;
 	}
-	
-	public List<List<Integer>> getClassMethods() {
-		return classMethods;
+
+	public List<List<Integer>> getClassMethodAddresses() {
+
+		return methodAddresses;
 	}
-	
+
+	public void addClassMethod(List<Integer> classMethodAddress) {
+
+		methodAddresses.add(classMethodAddress);
+	}
+
 }
