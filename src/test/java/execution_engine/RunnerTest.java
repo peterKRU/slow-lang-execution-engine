6aac55bfa2eff9f4e04aeed81ebda99d63ecfea7
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-10 15:08:38 +0200

    test: add unit tests for Logger

--- src/test/java/execution_engine/RunnerTest.java
+++ src/test/java/execution_engine/RunnerTest.java
@@ -1,9 +1,12 @@
 package execution_engine;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RunnerTest {
@@ -11,9 +14,19 @@ class RunnerTest {
 	
 	
 	@Test
-	void test() throws IOException {
-		
-		assertTrue(true);
+	@DisplayName("Test Runner with default input: should log 11")
+	
+	final void testRunnerWithDefaultTest() throws IOException {
+
+		Runner.runDefaultTest();
+
+		List<String> log = ChainedVM.logger.getLog();
+
+		assumeTrue(log.size() == 1);
+
+		Integer resultValue = ResourceReader.readInteger(log.get(0));
+
+		assertTrue(resultValue == 11);
 	}
 
 }
