e6656c0fd1357816e8582207323322e79ba79c8e
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-10 19:33:28 +0200

    feat: add DEFAULT_LOGGER_FILE_NAME got constants table

--- src/main/java/execution_engine/ConstantsTable.java
+++ src/main/java/execution_engine/ConstantsTable.java
@@ -7,4 +7,5 @@ public class ConstantsTable {
 	public static final int DEFAULT_LOCAL_STORAGE_SIZE = 1000;
 	
 	public static final String DEFAULT_TEST_FILE_NAME = "test_classes_compiled.txt";
+	public static final String DEFAULT_LOG_FILE_NAME = "log.txt";
 }
