091a86cffa8128102198551b26943dfcdfdc956b
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-10 15:09:10 +0200

    test: add unit tests for MemoryManager

--- src/test/java/execution_engine/MemoryManagerTest.java
+++ src/test/java/execution_engine/MemoryManagerTest.java
@@ -10,13 +10,12 @@ import org.junit.jupiter.api.Test;
 class MemoryManagerTest {
 
 	@Test
-	@DisplayName("Test loadInstructionsMethod: loaded instructions array size should be 28")
-	final void testMemoryManager() throws IOException {
+	@DisplayName("Test MemoryManager instantiation: should load instructions with length 76")
+	final void testMemoryManagerInstantiation() throws IOException {
 
-		MemoryManager memoryManager = new MemoryManager("test_compiled.txt");
-		int[] instructions = memoryManager.loadInstructions();
-				
-		assertTrue(instructions.length == 28);
+		MemoryManager memoryManager = new MemoryManager("test_classes_compiled.txt");
+		
+		assertTrue(memoryManager.loadInstructions().length == 76);
 	}
 
 }
