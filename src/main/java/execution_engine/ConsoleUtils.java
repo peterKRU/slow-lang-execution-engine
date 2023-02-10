96757f8124959db55268d589b558e3ba1539c472
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-10 19:35:52 +0200

    feat: add ConsoleUtils data holder class with message and command constants

---
+++ src/main/java/execution_engine/ConsoleUtils.java
@@ -0,0 +1,11 @@
+package execution_engine;
+
+public class ConsoleUtils {
+	
+	public static final String DEFAULT_STATIC_MESSAGE = ": ";
+	public static final String NOT_RECOGNIZED = "Command not recognized. ";	
+	
+	public static final String EXIT = "exit";
+	public static final String COMPILE = "compile";
+	public static final String TEST = "test";
+}
