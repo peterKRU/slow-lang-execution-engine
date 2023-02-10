ae38ca42956216c6b4ee85c434cae292812747c7
Peter Krastanov <peter.krastanow@gmail.com>
2023-02-10 15:07:16 +0200

    feat: add DEFAULT_TEST_FILE_NAME to constants table

--- src/main/java/execution_engine/ConstantsTable.java
+++ src/main/java/execution_engine/ConstantsTable.java
@@ -5,4 +5,6 @@ public class ConstantsTable {
 	public static final int DEFAULT_GLOBAL_STORAGE_SIZE = 1000;
 	public static final int DEFAULT_STACK_SIZE = 1000;
 	public static final int DEFAULT_LOCAL_STORAGE_SIZE = 1000;
+	
+	public static final String DEFAULT_TEST_FILE_NAME = "test_classes_compiled.txt";
 }
